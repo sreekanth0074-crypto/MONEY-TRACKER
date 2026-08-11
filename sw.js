1865aea35ab5d2e32d36631e71e24c605922a609
sreekanth0074-crypto <sreekanth0074@gmail.com>
2026-08-12 02:17:39 +0530

    Upgrade cache version and manage old caches
    Updated cache version and added activation event to delete old caches.

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = 'money-tracker-v1';
+const CACHE_NAME = 'money-tracker-v2';
 const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
